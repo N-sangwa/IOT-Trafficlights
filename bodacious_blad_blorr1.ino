3b8f4a6bcec1d6ad2ed1b614469bb23fd90712a9
N-sangwa <99467713+N-sangwa@users.noreply.github.com>
2022-03-11 17:06:39 +0300

    Add files via upload

---
+++ bodacious_blad_blorr1.ino
@@ -0,0 +1,19 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(1,HIGH);
+  delay(1000);
+  digitalWrite(1,LOW);
+  delay(1000);
+}
